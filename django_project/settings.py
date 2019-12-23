6ed37a890038c35eeca71c4b6d318a9d37fb16c1
Ian Riva <ianriva96.ir@gmail.com>
2019-12-23 00:00:18 -0300

    cleanup added

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_cleanup.apps.CleanupConfig',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
