72ff31160f23fc36bdaed3a831ffe6a5e2c5dfd3
Ian Riva <ianriva96.ir@gmail.com>
2019-12-27 22:53:49 -0300

    created s3 bucket storage

--- users/models.py
+++ users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+import io
+from django.core.files.storage import default_storage as storage
 from PIL import Image
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
@@ -11,9 +14,27 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
+        img_read = storage.open(self.image.name, 'r')
+        img = Image.open(img_read)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            in_mem_file = io.BytesIO()
+            img.convert('RGB').save(in_mem_file, format='JPEG')
+            img_write = storage.open(self.image.name, 'w+')
+            img_write.write(in_mem_file.getvalue())
+            img_write.close()
+
+        img_read.close()
+    #resize img using pillow
+    #keep it deactivate as long as using s3 bucket
+    '''def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+            img.save(self.image.path)'''
