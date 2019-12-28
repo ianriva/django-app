1a0c955727f27f3c9d8e6575b1e33ee94919a388
Ian Riva <ianriva96.ir@gmail.com>
2019-12-28 01:36:52 -0300

    updated requirements

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django_cleanup.apps.CleanupConfig',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
